ab32e697b22c4e851c048a8ebf69bd25cb7e0f64
alanalzi <117884402+alanalzi@users.noreply.github.com>
2022-12-15 13:00:03 +0700

    Add files via upload

---
+++ UG_13_71220824/1_B_71220824.py
@@ -0,0 +1,29 @@
+import turtle
+s = turtle.Screen()
+t = turtle.Turtle()
+
+# t.penup()
+# t.goto(-167,-190)
+# t.pendown()
+# t.left(90)
+# t.forward(50)
+# t.circle(-55,220)
+
+#angka pertama
+t.penup()
+t.goto(-10,-90)
+t.pendown()
+t.circle(30)
+t.penup()
+t.goto(-10,-32)
+t.pendown()
+t.circle(30)
+
+# angka kedua
+t.penup()
+t.goto(-32,-32)
+t.pendown()
+t.circle(-60,55)
+# t.left(-100)
+
+s.exitonclick
